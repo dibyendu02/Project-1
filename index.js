bfc806a16a0e110e66350f761ff3494e5720448b
Dibyendu Das <get2dibyendu@gmail.com>
2023-03-15 15:16:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var string = "English literature, Article review, Article writing, Article Publishing (Scholar), Thesis/dissertation, Law, Engineering implementations etc.";
+var str = string.split("");
+var el = document.getElementById('text');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 90);
+})();
